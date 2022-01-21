34dfa51a394095a9c053918ea170a0c1a1fbdcd5
sandeep sahoo <sandeepsahoo542@gmail.com>
2022-01-21 15:04:32 +0530

    Main file
    This is the main file which will be compiled and executed , this file contains the main game loop

---
+++ Gangwar.cpp
@@ -0,0 +1,20 @@
+
+/*
+  structure for the man 
+*/
+
+
+/*
+  player class having the functions necessary 
+*/
+
+
+/*
+  other game functions 
+*/
+
+int main(){
+/*
+  Make the game loop here 
+*/
+}
